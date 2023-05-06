91195fdc377e02ae52fb7454fdd8efe945ce4303
Gyu Min Hwang <rbals1593@naver.com>
2023-05-06 12:08:49 +0900

    1commit

--- api/_controller/todoController.js
+++ api/_controller/todoController.js
@@ -1,14 +1,14 @@
 const db = require("../../plugins/mysql");
 const TABLE = require("../../util/TABLE");
 const STATUS = require("../../util/STATUS");
-const { resData, currentTime, isEmpty } = require("../../util/lib");
+const { resData, isEmpty } = require("../../util/lib");
 const moment = require("../../util/moment");
 
 //전체 row 갯수
 const getTotal = async () => {
   // const getTotal = async function () {
   try {
-    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.TODO}`;
+    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.TODO} WHERE done='N'`;
     const [[{ cnt }]] = await db.execute(query);
     return cnt;
   } catch (e) {
@@ -47,9 +47,9 @@ const getList = async (req) => {
     let where = "";
     if (lastId) {
       // 0은 false
-      where = `WHERE id < ${lastId}`;
+      where = `AND id < ${lastId}`;
     }
-    const query = `SELECT * FROM ${TABLE.TODO} ${where} order by id desc limit 0, ${len}`;
+    const query = `SELECT * FROM ${TABLE.TODO} WHERE done='N' ${where} order by id desc limit 0, ${len}`;
     const [rows] = await db.execute(query);
     return rows;
   } catch (e) {
@@ -64,7 +64,7 @@ const getList = async (req) => {
 const todoController = {
   // create
   create: async (req) => {
-    const { title, done } = req.body;
+    const { title } = req.body;
     if (isEmpty(title) || isEmpty(done)) {
       return resData(
         STATUS.E100.result, 
@@ -73,14 +73,15 @@ const todoController = {
     }
 
     try {
-      const query = `INSERT INTO todo (title, done) VALUES (?,?)`;
-      const values = [title, done];
+      const query = `INSERT INTO todo (title) VALUES (?)`;
+      const values = [title];
       const [rows] = await db.execute(query, values);
       if (rows.affectedRows == 1) {
         return resData(
           STATUS.S200.result,
           STATUS.S200.resultDesc,
           moment().format('LT'),
+          { id: rows.insertId }
         );
       }
     } catch (e) {
@@ -117,10 +118,7 @@ const todoController = {
     const { id } = req.params; // url /로 들어오는것
     const { title, done } = req.body;
     if (isEmpty(id) || isEmpty(title) || isEmpty(done)) {
-      return resData(
-        STATUS.E100.result, 
-        STATUS.E100.resultDesc, 
-        moment().format('LT'));
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
     }
 
     try {
@@ -136,10 +134,7 @@ const todoController = {
       }
     } catch (e) {
       console.log(e.message);
-      return resData(
-        STATUS.E300.result, 
-        STATUS.E300.resultDesc, 
-        moment().format('LT'));
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
     }
   },
 
@@ -181,91 +176,7 @@ const todoController = {
     return rows;
   },
 
-  // reset: async(req) => {
-  //   const query = 'TRUNCATE TABLE ${TABLE.TODO}';
-  //   // const [rows] = await db.execute(query);
-  //   try {
-  //     for(i=0; i < TABLE.TODO.len+1; i++){
-  //       const query = `INSERT INTO todo (title) VALUES (?)`;
-  //       const values = [i];
-  //       const [rows] = await db.execute(query, values);
-  //       if (rows.affectedRows == 1) {
-  //         return resData(
-  //           STATUS.S200.result,
-  //           STATUS.S200.resultDesc,
-  //           moment().format('LT'),
-  //           );
-  //         }
-  //       }
-  //     }
-  //    catch (e) {
-  //     console.log(e.message);
-  //     return resData(
-  //       STATUS.E300.result, 
-  //       STATUS.E300.resultDesc, 
-  //       moment().format('LT'));
-  //   }
-  // }  
 
-  // reset: async (req) => {
-  //   const list = await getList();
-  //   try {
-  //     const query = `TRUNCATE TABLE todo;`;
-  //     await db.query(query);
-  //     console.log("삭제")
-  //     //console.log(db)
-  //     let insquery = `INSERT INTO todo (title) VALUES `;
-  //     for (let i = 1; i <= list.length; i++) {
-  //       insquery += `('${i}'), `;
-  //     }
-  //     insquery = insquery.slice(0, -2); // 마지막 쉼표 제거
-  
-  //     await db.query(insquery);
-  //     console.log("재생성")
-      
-
-  //     return resData(
-  //       STATUS.S200.result,
-  //       STATUS.S200.resultDesc,
-  //       moment().format('LT')
-  //     );
-  //   } catch (e) {
-  //     console.log(e.message);
-  //     return resData(
-  //       STATUS.E300.result,
-  //       STATUS.E300.resultDesc,
-  //       moment().format('LT')
-  //     );
-  //   }
-  // }
-  
-  // reset: async (req) => {
-  //   const list = await getList();
-  //   try {
-  //     const query = `TRUNCATE TABLE ${TABLE.TODO};`;
-  //     await db.execute(query);
-  
-  //     let insquery = `INSERT INTO ${TABLE.TODO} (title) VALUES `;
-  //     for (let i = 1; i <= list.length; i++) {
-  //       insquery += `('${i}'), `;
-  //     }
-  //     insquery = insquery.slice(0, -2); // 마지막 쉼표 제거
-  
-  //     await db.execute(insquery);
-  //     return resData(
-  //       STATUS.S200.result,
-  //       STATUS.S200.resultDesc,
-  //       moment().format('LT')
-  //     );
-  //   } catch (e) {
-  //     console.log(e.message);
-  //     return resData(
-  //       STATUS.E300.result,
-  //       STATUS.E300.resultDesc,
-  //       moment().format('LT')
-  //     );
-  //   }
-  // }
   reset: async (req) => {
     const {title, len} = req.body;
 
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const config = require('./config').development;
-const express = require('express');
-const http = require('http');
+const config = require("./config")[process.env.NODE_ENV];
+const express = require("express");
+const http = require("http");
 
 const app = express();
 const port = config.PORT;
-const cors = require('cors');
+// const router = express.Router();
+const cors = require("cors");
 
 //cors
-let corsOptions = {
-	origin: '*', 
-	credential: true, 
+const corsOptions = {
+  origin: "http://localhost:8181", // 출처 허용 옵션
+  credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 };
+
 app.use(cors(corsOptions));
+
 //body parser
 app.use(express.json());
-app.use(express.urlencoded({extended : true }));
+app.use(express.urlencoded({ extended: true }));
 
 //autoRouter
-const autoRoute = require('./autoRoute');
-autoRoute('/api',app);
+const autoRoute = require("./autoRoute");
+autoRoute("/api", app);
 
 //server
 const webServer = http.createServer(app);
-webServer.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-})
+webServer.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
